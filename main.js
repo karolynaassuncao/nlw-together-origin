f33e1982ae8d0493d6ab52e1105b6f7639ff5da9
karolyna.assuncao <karolyna.assuncao@wapstore.com.br>
2022-04-16 22:06:21 -0300

    finalização de projeto nlw 6

--- main.js
+++ main.js
@@ -21,10 +21,93 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY >= navHeight){
+function changeScrollWhenScroll() {
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
-  }else {
+  } else {
     header.classList.remove('scroll')
   }
+}
+
+// -------------------- SWIPER -----------------------
+// Slider com PerVier de 1 card, com paginação (bolinhas), rolagem com mouse e com setas do teclado
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
+
+// -------------------- SCROLLREVEAL -----------------------
+// Mostrar elementos quando der scroll na página
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonial,
+  #contact .text, #contact .links,
+  #footer .brand, #footer .social
+`,
+  { interval: 100 }
+)
+
+/* -------------------- ARROW ----------------------- */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// Menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd){
+      document
+        .querySelector('nav ul li a[href*=' + sectionId +']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId +']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// When scroll - chamando funções de scroll
+window.addEventListener('scroll', function () {
+  changeScrollWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+ 
